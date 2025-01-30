7a07a4f3d6c8e3dede1b5492366ecdcb9517cec5
Julio Cesar Tertuliano de Barros Junior <julio.b.junior.23708@aluno.facape.br>
2025-01-30 18:35:18 -0300

    criaçao do diagram.json e do wokwi.toml e implementaçao do codigo em C da atividado de semaforo temporizador

--- SemaforoTemporizador.c
+++ SemaforoTemporizador.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
+#define Led_vermelho 13
+#define Led_Amarelo 12
+#define Led_Verde 11
 
+volatile int state = 0;
+
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+    gpio_put(Led_vermelho, state == 0);
+    gpio_put(Led_Amarelo, state == 1);
+    gpio_put(Led_Verde, state == 2);
+
+    state = (state +1) % 3;
+    return true;
+};
 
 int main()
 {
     stdio_init_all();
 
+    gpio_init(Led_vermelho);
+    gpio_set_dir(Led_vermelho, GPIO_OUT);
+        gpio_init(Led_Amarelo);
+    gpio_set_dir(Led_Amarelo, GPIO_OUT);
+        gpio_init(Led_Verde);
+    gpio_set_dir(Led_Verde, GPIO_OUT);
+
+    struct repeating_timer timer;
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+
     while (true) {
-        printf("Hello, world!\n");
+        printf("Semaforo Funcionando...\n");
         sleep_ms(1000);
     }
 }
